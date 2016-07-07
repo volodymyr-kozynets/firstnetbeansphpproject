b45a985e198dd1c0e2690c66b7d037221e4996e6
Kozynets Volodymyr <vladdimir@inbox.ru>
2016-07-07 21:27:42 +0300

    Добавлена функция E-mail оповещения при регистрации нового пользователя

--- controllers/administrations.controller.php
+++ controllers/administrations.controller.php
@@ -2,6 +2,11 @@
 
 class AdministrationsController extends Controller{
     
+    public static $to;
+    public static $subject;
+    public static $message;
+    public static $headers;
+
     public function __construct($data = array()) {
             parent::__construct($data); // вызываем конструктор из родительского класса с атрибутом data в кот.храняться данные для представления
             $this->model = new Administration();
@@ -11,6 +16,28 @@ class AdministrationsController extends Controller{
         if($_POST){
             if($this->model->saveUser($_POST)){
                     Session::setFlash('Пользователь добавлен!');
+                    self::$to = $_POST['newemail'];
+                    self::$subject = 'New User';
+                    self::$message = '
+                            <html>
+                                <title>'.self::$subject.'</title>
+                            <head>
+                            </head>
+                            <body>
+                            <p style="font-weight:bold">New user - 
+                            '.$_POST['newlogin'].'
+                            was added!</p>
+                            <p>Details: 
+                            '.$_POST['newfullname'].'<br/>
+                            '.$_POST['newemail'].'<br/>
+                            '.$_POST['newrole'].'<br/>
+                            '.$_POST['newpassword'].'</p>
+                            </body>
+                            </html>
+                            ';
+                    self::$headers = "Content-type: text/html; charset=utf-8 \r\n";
+                    self::$headers .= "From: zenonbambolino@bestplace.in.ua \r\n";
+                    mail(self::$to,  self::$subject,  self::$message,  self::$headers);
             }
         }
     }
